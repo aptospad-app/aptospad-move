df77a9387af3811968104b108d100f6e8d72eba9
AaronNg102 <aaron.deepblock@gmail.com>
2022-11-16 16:56:50 +0700

    add getConfig

--- api.sh
+++ api.sh
@@ -36,4 +36,7 @@ PAD_PATH=`pwd`
 ##public entry fun withdrawAptosPad(admin: &signer, debit: address, amount: u64)
 #aptos move run --profile "$PROFILE" --function-id "$APTOSPAD_ADDR::scripts::withdrawAptosPad"  --args "address:<addr>" "u64:10000000"
 
-curl "https://fullnode.testnet.aptoslabs.com/v1/accounts/$RESOURCE_ADDR/resource/$APTOSPAD_ADDR::aptospad_swap::LaunchPadRegistry"
+#curl "https://fullnode.testnet.aptoslabs.com/v1/accounts/$RESOURCE_ADDR/resource/$APTOSPAD_ADDR::aptospad_swap::LaunchPadRegistry"
+
+curl "https://fullnode.testnet.aptoslabs.com/v1/accounts/$RESOURCE_ADDR/resource/$APTOSPAD_ADDR::config::ApttSwapConfig"
+
